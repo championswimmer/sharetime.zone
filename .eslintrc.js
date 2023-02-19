f91f635febe2fa6e2722a0cfe9ad06099b5233e2
Arnav Gupta <championswimmer@gmail.com>
2023-02-19 23:19:35 +0530

    add lint setup
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard',
+    '@vue/typescript/recommended',
+    '@nuxtjs/eslint-config-typescript'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-unused-vars': 0
+  }
+}
---
+++ lint-stages.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  '*.{js,jsx,vue,ts,tsx}': 'npm run lint:fix'
+};
