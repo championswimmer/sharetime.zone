16627243de98fa6b300c95b30a53f2f947fda661
Arnav Gupta <championswimmer@gmail.com>
2023-02-19 19:46:04 +0530

    fix: fonts back to js import mechanism; this is faster
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import '@/styles/app.scss'
+import '@/styles/fonts.css'
 
 createApp(App)
   .use(router)
