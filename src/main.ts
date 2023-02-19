41e89bfc6fc4a101f8a0da0796ee7491ee83f595
Arnav Gupta <championswimmer@gmail.com>
2023-02-19 19:44:21 +0530

    perf: pinia was not needed; duh
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import { createPinia } from 'pinia'
 import '@/styles/app.scss'
 
 createApp(App)
   .use(router)
-  .use(createPinia())
   .mount('#app')
