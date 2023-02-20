61222cd837024354cbc99527a2d8b40891484302
Arnav Gupta <championswimmer@gmail.com>
2023-02-20 17:58:09 +0530

    hygine: add favicons
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,13 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       meta: [
         { name: 'description', content: 'Share time in different timezones easily' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' }
       ]
     }
   },
