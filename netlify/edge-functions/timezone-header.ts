0d0b2ed18e8471d57984e689a056dbc41a01f230
Arnav Gupta <championswimmer@gmail.com>
2023-02-21 18:32:17 +0530

    deno fixes
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- netlify/edge-functions/timezone-header.ts
+++ netlify/edge-functions/timezone-header.ts
@@ -1,13 +1,6 @@
 
-import type { Context } from '@netlify/edge-functions'
-import { EdgeRequest } from '@netlify/edge-functions/dist/types/request'
+import { Context } from 'https://edge.netlify.com'
 
-export default async (request: EdgeRequest, context: Context) => {
+export default async (request: Request, context: Context) => {
   await request.headers.set('x-user-timezone', context.geo.timezone)
-}
-
-declare module '@netlify/edge-functions/dist/types/geo' {
-  interface Geo {
-    timezone: string;
-  }
 }
