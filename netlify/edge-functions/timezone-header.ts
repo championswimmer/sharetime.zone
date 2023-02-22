ef154e79e2c624a0ef8efb3edae067302bc5ed0f
Arnav Gupta <championswimmer@gmail.com>
2023-02-22 20:18:35 +0530

    fix edge functions
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- netlify/edge-functions/timezone-header.ts
+++ netlify/edge-functions/timezone-header.ts
@@ -2,5 +2,5 @@
 import { Context } from 'https://edge.netlify.com'
 
 export default async (request: Request, context: Context) => {
-  await request.headers.set('x-user-timezone', context.geo.timezone)
+  await request.headers.set('x-user-timezone', context.geo.timezone ?? '')
 }
