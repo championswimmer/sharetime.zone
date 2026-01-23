941986ee6079849de9b8363fce0d5080ab58aca5
Arnav Gupta <dev@championswimmer.in>
2026-01-23 01:58:38 +0000

    add examples for sharing time in 12-hour format in HowToUse.vue

--- src/components/HowToUse.vue
+++ src/components/HowToUse.vue
@@ -31,6 +31,9 @@
         <p class="has-text-weight-bold">
           Examples
         </p>
+        <p>
+          Supported formats: 24-hour (e.g. <code>/PST/1500</code>) or 12-hour with <code>am</code>/<code>pm</code> (e.g. <code>/PST/3pm</code>, <code>/Europe/Berlin/4:45pm</code>).
+        </p>
         <ul>
           <li class="my-2">
             Share the time <code>3:00PM</code> of <code>PST (Pacific Standard Time)</code>
@@ -42,6 +45,18 @@
             Share the time <code>11:00AM</code> of <code>Europe/Berlin</code>
             <br>
             <a href="/Europe/Berlin/1100">sharetime.zone/Europe/Berlin/1100</a>
+          </li>
+
+          <li class="my-2">
+            Share the time <code>3:00PM</code> of <code>PST</code> (12-hour)
+            <br>
+            <a href="/PST/3pm">sharetime.zone/PST/3pm</a>
+          </li>
+
+          <li class="my-2">
+            Share the time <code>4:45PM</code> of <code>Europe/Berlin</code> (12-hour)
+            <br>
+            <a href="/Europe/Berlin/4:45pm">sharetime.zone/Europe/Berlin/4:45pm</a>
           </li>
         </ul>
       </div>
